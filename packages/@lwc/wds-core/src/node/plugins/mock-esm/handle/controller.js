1b5b055e835bf6fda1c40693d169b6f32f8d06e0
abhagta <abhagta@salesforce.com>
2024-05-31 13:19:08 +0530

    fix: code clean

--- packages/@lwc/wds-core/src/node/plugins/mock-esm/handle/controller.js
+++ packages/@lwc/wds-core/src/node/plugins/mock-esm/handle/controller.js
@@ -7,31 +7,13 @@ import { MOCK_CONTROLLER_PREFIX, MOCK_STUB_PREFIX } from '../const.js';
 const LEXER_ABS_PATH = resolveSync('es-module-lexer/dist/lexer.asm.js', {
   baseDir: toolkitSrcPath,
 });
-const ESPRIMA_ABS_PATH = resolveSync('escodegen/escodegen.js', {
-  baseDir: toolkitSrcPath,
-});
 const BROWSER_SSR_ABS_PATH = pathResolve(toolkitSrcPath, './browser/ssr/index.js');
+
 const relFromRoot = (absPath) => (rootDir) => pathRelative(rootDir, absPath);
 const lexerAbsUrl = relFromRoot(LEXER_ABS_PATH);
-const esprimaAbsUrl = relFromRoot(ESPRIMA_ABS_PATH);
 const browserSsrUrl = relFromRoot(BROWSER_SSR_ABS_PATH);
-// I passed the original content of the mocked file here so while mocking instead of mocking with
-// moduleCode { which causes the issue in partial mocking as it only tracks mocked values } we merge the code
-// from original content and mockedCode which would have all the exports from original module code and update
-// values of mocks updated in partial mocking
-/* I thought of doing it in two ways
-1. Create a mergeExports func in util file where I you pass orignal content of module and mocked code and 
-it outputs a merged code.I have written that func in util.js but not able to successfully import and use it in the script below.
-2. To implemeent the mergeExports func in the script below only and for that I would need to import esprima and codegen.
-*/
-const buildMockController = (
-  resolvedOrUnresolvedImport,
-  exportedNames,
-  rootDir,
-  query,
-  importExists,
-  content,
-) => `
+
+const buildMockController = (resolvedOrUnresolvedImport, exportedNames, rootDir, query) => `
 import { parse as parseEsm } from '/${lexerAbsUrl(rootDir)}';
 import { __mock__ } from '${resolvedOrUnresolvedImport}${query}';
 import { mock as mockSSR, resetMock as resetMockSSR } from '/${browserSsrUrl(rootDir)}${query}';
@@ -49,16 +31,15 @@ function assertHasSameExports(newExportsArr) {
 }
 
 export default async function mockModule(moduleCode) {
-  const originalModuleCode = "${content}";
-  const [, _originalExports] = parseEsm(originalModuleCode);
-  const originalExportedNames = _originalExports.map(exp => exp.n);
   const [, _exports] = parseEsm(moduleCode);
   const exportedNames = _exports.map(exp => exp.n);
   assertHasSameExports(exportedNames);
+
   const dataUri = 'data:text/javascript,' + encodeURIComponent(moduleCode);
   await __mock__.useImport(dataUri);
   await mockSSR('${resolvedOrUnresolvedImport}', dataUri);
 }
+
 mockModule.reset = async () => {
   __mock__.resetAll();
   await resetMockSSR('${resolvedOrUnresolvedImport}');
