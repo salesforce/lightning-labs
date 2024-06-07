142e252aa09457f3f05cb2b39268db0fd10693c9
Dale Bustad <dbustad@salesforce.com>
2024-06-07 14:25:30 -0700

    chore: fix bad assumption about i18n reference implementation

--- packages/@lwc/wds-core/src/node/plugins/platform/i18n.js
+++ packages/@lwc/wds-core/src/node/plugins/platform/i18n.js
@@ -8,11 +8,20 @@ export default ({ rootDir, locale }) => ({
   name: 'ecosystem-i18n',
   resolveImport({ source }) {
     if (source.startsWith('@salesforce/i18n/')) {
-      const moduleAbsPath = resolveSync(
-        path.join('@salesforce/i18n/dist', locale, source.slice(17)),
-        { basedir: __dirname },
-      );
-      return `/${path.relative(rootDir, moduleAbsPath)}`;
+      try {
+        const moduleAbsPath = resolveSync(
+          path.join('@salesforce/i18n/dist', locale, source.slice(17)),
+          { basedir: __dirname },
+        );
+        return `/${path.relative(rootDir, moduleAbsPath)}`;
+      } finally {
+        // Why the try/finally here? It turns out that the `@salesforce/i18n`
+        // reference implementation is incomplete and does not include everything
+        // that is present when imported on the Salesforce platform. Consequently,
+        // We can't assume that the above `resolveSync` will actually succeed.
+        //
+        // Therefore we need to return undefined from this function.
+      }
     }
   },
 });
