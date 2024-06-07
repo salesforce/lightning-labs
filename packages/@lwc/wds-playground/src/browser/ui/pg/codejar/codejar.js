d3d92bd6733fd1399869fbc91357eb3f3d360ea5
Matheus Cardoso <matheus@cardo.so>
2024-06-07 17:46:29 -0300

    feat: basic highlighting for ssr markup (#14)
    * feat: basic highlighting for ssr markup
    
    * fix(syntax-highlighting): preserve self-closing tags

--- packages/@lwc/wds-playground/src/browser/ui/pg/codejar/codejar.js
+++ packages/@lwc/wds-playground/src/browser/ui/pg/codejar/codejar.js
@@ -1,5 +1,6 @@
 import { CodeJar } from 'codejar';
 import { LightningElement, api } from 'lwc';
+import htmlPlugin from 'prettier/esm/parser-html.mjs';
 
 export default class Codejar extends LightningElement {
   @api nomargin = false;
@@ -28,7 +29,13 @@ export default class Codejar extends LightningElement {
     }
     this.hasRendered = true;
     const editorEl = this.template.querySelector('.editor');
-    this.jar = new CodeJar(editorEl, () => {}, { tab: '  ' });
+    this.jar = new CodeJar(
+      editorEl,
+      (editor) => {
+        editor.innerHTML = highlightHtmlText(editor.textContent);
+      },
+      { tab: '  ' },
+    );
     this.jar.updateCode(this.content);
     editorEl.addEventListener('input', () => (this._content = this.jar.toString()));
   }
@@ -38,4 +45,43 @@ export default class Codejar extends LightningElement {
       evt.stopPropagation();
     }
   }
+}
+
+/**
+ * @param {string} text
+ * @returns {string}
+ */
+function highlightHtmlText(text) {
+  const ast = htmlPlugin.parsers.html.parse(text);
+  return highlightHtmlAst(ast.children);
+}
+
+/**
+ * @param {any[]} ast
+ * @returns {string}
+ */
+function highlightHtmlAst(ast) {
+  let result = '';
+  for (const node of ast) {
+    if (node.type === 'text') {
+      result += node.value;
+    } else {
+      result += `<span class="highlight-tag">&lt;${node.name}</span>`;
+      for (const attr of node.attrs) {
+        result += `<span class="highlight-attr"> ${attr.name}</span>`;
+        if (attr.value) {
+          result += '<span class="highlight-equals">=</span>';
+          result += `<span class="highlight-value">"${attr.value}"</span>`;
+        }
+      }
+      if (node.tagDefinition.isVoid) {
+        result += '<span class="highlight-tag"> /&gt;</span>';
+      } else {
+        result += '<span class="highlight-tag">&gt;</span>';
+        result += highlightHtmlAst(node.children);
+        result += `<span class="highlight-tag">&lt;/${node.name}&gt;</span>`;
+      }
+    }
+  }
+  return result;
 }
