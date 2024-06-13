7a14596161dfd1417b684e10dd5399d398f43885
Dale Bustad <dbustad@salesforce.com>
2024-06-12 18:10:33 -0700

    chore: split child into badChild for demonstration purposes

--- example/src/modules/x/child/child.js
+++ example/src/modules/x/child/child.js
@@ -2,9 +2,4 @@ import { LightningElement, api } from 'lwc';
 
 export default class App extends LightningElement {
   @api message = '';
-  get badmessage() {
-    // Intentionally rendering differently in SSR & CSR to demonstrate
-    // the playground functionality.
-    return import.meta.env.SSR ? `${this.message} oops SSR oops` : this.message;
-  }
 }
