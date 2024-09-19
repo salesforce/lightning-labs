83cb79ddc342b6e0f64e7cfb5e00513d6509eaa0
rax-it <rakshit.verma@salesforce.com>
2024-09-19 11:06:45 -0700

    Update packages/@lwc/state/src/index.ts
    Co-authored-by: Dale Bustad <dbustad@salesforce.com>

--- packages/@lwc/state/src/index.ts
+++ packages/@lwc/state/src/index.ts
@@ -158,8 +158,8 @@ export const defineState: DefineState = (defineStateCallback) => {
         return this._value;
       }
 
-      // TODO: instances of this class must take the shape of `ContextProvider` and `ContextConsumer` in
-      //       the same way that it takes the shape/implements `Signal`
+      // TODO: W-16769884 instances of this class must take the shape of `ContextProvider` and 
+      //       `ContextConsumer` in the same way that it takes the shape/implements `Signal`
     }
 
     return new StateManagerSignal();
