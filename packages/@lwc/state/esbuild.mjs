617fc9b64f0e6ea1fc8223dddd5b6fe90fac6009
rax-it <rakshit.verma@salesforce.com>
2024-10-30 13:24:57 -0700

    chore: update banner and footer

--- packages/@lwc/state/esbuild.mjs
+++ packages/@lwc/state/esbuild.mjs
@@ -1,12 +1,28 @@
 import * as esbuild from 'esbuild';
+import { readFileSync } from 'node:fs';
+
+const pkg = JSON.parse(readFileSync('./package.json', 'utf8'));
+
+/**
+ * eslint-disable is required for module to bypass eslint rules in core
+ */
+const BANNER = `
+/* eslint-disable */
+/**
+ * This file is generated from repository: ${pkg.repository?.url}
+ * module: ${pkg.repository?.directory} 
+ */`;
 
 await esbuild.build({
   banner: {
-    js: '/* eslint-disable */',
+    js: BANNER,
   },
   entryPoints: ['./src/core.ts'],
   bundle: true,
   external: ['lwc'],
   outfile: 'dist/bundle.js',
   format: 'esm',
+  footer: {
+    js: `/* @lwc/state v${pkg.version} */`,
+  },
 });
