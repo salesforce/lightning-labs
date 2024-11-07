b756268c5a055d0230b080044c38f438ef51ca0b
rax-it <rakshit.verma@salesforce.com>
2024-11-07 14:33:54 -0800

    chore: cleanup

--- example/src/modules/x/contextChild/contextChild.js
+++ example/src/modules/x/contextChild/contextChild.js
@@ -1,9 +1,7 @@
-import { api } from 'lwc';
 import { ContextfulLightningElement } from '@lwc/state/context';
 import childStateFactory from 'x/childState';
 
 export default class ContextChild extends ContextfulLightningElement {
-  @api
   childState = childStateFactory();
 
   get nameProvidedByParent() {
